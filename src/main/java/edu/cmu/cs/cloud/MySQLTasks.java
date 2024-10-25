209ec639fb7078a6c1b8c016f8ed68cc8df15d66
kevin0988459 <kevin0988459@gmail.com>
2024-10-25 01:23:51 -0400

    q6

--- src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MySQLTasks.java
@@ -249,7 +249,7 @@ public class MySQLTasks {
      */
     private static void q5() {
         String sql = "SELECT name FROM businesses " +
-                     "WHERE neighborhood = 'South Side' AND name LIKE '%Coast%' " +
+                     "WHERE neighborhood = 'South Side' AND name LIKE BINARY '%Coast%' " +
                      "AND business_id NOT IN (SELECT business_id FROM checkins)";
         executeDataManipulationQuery(sql);
     }
@@ -267,10 +267,10 @@ public class MySQLTasks {
      * You are only allowed to edit the sql.
      */
     private static void q6() {
-        String sql = "SELECT r.user_id FROM reviews r " +
+        String sql = "SELECT DISTINCT r.user_id " +
+                     "FROM reviews r " +
                      "JOIN tips t ON r.user_id = t.user_id " +
-                     "WHERE r.cool = (SELECT MAX(cool) FROM reviews) " +
-                     "GROUP BY r.user_id";
+                     "WHERE r.cool = (SELECT MAX(cool) FROM reviews)";
         executeDataManipulationQuery(sql);
     }
 
