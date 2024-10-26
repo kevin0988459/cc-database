aa14e3b3b56713d665c6886795d289a742645896
kevin0988459 <kevin0988459@gmail.com>
2024-10-26 18:13:59 -0400

    q10

--- src/main/java/edu/cmu/cs/cloud/MongoDBTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MongoDBTasks.java
@@ -210,7 +210,7 @@ public class MongoDBTasks {
         Bson cityFilter = eq("city", "Pittsburgh");
         Bson hoursFilter = regex("hours.Friday", "17:00");
         Bson deliveryFilter = regex("attributes", "'RestaurantsDelivery':\\s*true", "i");
-        Bson query = and(nameFilter, neighborhoodFilter, cityFilter, hoursFilter);
+        Bson query = and(nameFilter, neighborhoodFilter, cityFilter,  deliveryFilter);
 
         try (MongoCursor<Document> cursor = mongoCollection.find(query).sort(Sorts.ascending("name")).iterator()) {
             if (!cursor.hasNext()) {
