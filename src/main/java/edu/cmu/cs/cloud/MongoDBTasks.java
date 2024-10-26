a247872545fa9b102715e27b058d6af44dbc6bf1
kevin0988459 <kevin0988459@gmail.com>
2024-10-26 16:32:59 -0400

    q9

--- src/main/java/edu/cmu/cs/cloud/MongoDBTasks.java
+++ src/main/java/edu/cmu/cs/cloud/MongoDBTasks.java
@@ -148,8 +148,13 @@ public class MongoDBTasks {
      * list and/or return type.
      */
     private static void q9() throws IOException {
-        throw new UnsupportedOperationException(
-                "Waiting to be implemented");
+        BSON neighborhood = regrex("neighborhood", "Shadyside", "i");
+        BSON categories = regrex("categories", "Asian Fusion", "i");
+        BSON wifi = eq("attributes.WiFi", "free");
+        BSON bikeParking = eq("attributes.BikeParking", "true");
+        BSON query = and(neighborhood, categories, wifi, bikeParking);
+
+        mongoCollection.find(query).forEach(document -> System.out.println(document.getString("name")));
     }
 
     /**
