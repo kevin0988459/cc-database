891bcc8672f3221a7805723f777fbb286a3f3211
kevin0988459 <kevin0988459@gmail.com>
2024-10-26 21:14:28 -0400

    redis

--- src/test/java/edu/cmu/cs/cloud/RedisTest.java
+++ src/test/java/edu/cmu/cs/cloud/RedisTest.java
@@ -12,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Usage:
